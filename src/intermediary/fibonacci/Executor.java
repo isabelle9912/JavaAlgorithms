a214bdfe0f69fe00b357ba702414bd805a9a1ea1
isabelle9912 <isabellesaahirah28@gmail.com>
2025-01-17 11:06:36 -0300

    fibonacci

---
+++ src/intermediary/fibonacci/Executor.java
@@ -0,0 +1,13 @@
+package intermediary.fibonacci;
+
+import java.util.Scanner;
+
+public class Executor {
+
+    public static final Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args){
+
+        System.out.println();
+    }
+}
