73bd94adaa8b9280d4f733283f2052fc99311dc1
isabelle9912 <isabellesaahirah28@gmail.com>
2025-01-19 10:36:29 -0300

    fibonacci sequence

--- src/intermediary/fibonacci/Executor.java
+++ src/intermediary/fibonacci/Executor.java
@@ -1,5 +1,6 @@
 package intermediary.fibonacci;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Executor {
@@ -8,6 +9,20 @@ public class Executor {
 
     public static void main(String[] args){
 
-        System.out.println();
+        System.out.println("Program to make fibonacci sequence");
+        System.out.print("Enter the number of elements you wish to make fibonacci sequence: ");
+        int n = sc.nextInt();
+        fibonacciSequence(n);
+    }
+
+    public static void fibonacciSequence(int n) {
+        int a = 1, b = 1, c;
+        System.out.print(a + ", " + b);
+        for(int i = 3; i <= n; i++) {
+            c = a + b;
+            System.out.print(", " + c);
+            a = b;
+            b = c;
+        }
     }
 }
